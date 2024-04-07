ca4ec975a84c7527ea5284ce4a432dd5c532dc19
mattveym <matej.tribula@gmail.com>
2024-04-07 12:03:16 +0200

    logic for one round added

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 console.log("cau svet")
+
+
+
+
+
+const playerSelection = 'rock'
+const computerSelection = getComputerChoice()
+
+function round(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a draw!"
+    } else if (
+        (playerSelection === "rock" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "rock")) {
+        return "You lose! " + computerSelection + " beats " + playerSelection
+    } else {
+        return "You win! " + playerSelection + " beats " + computerSelection
+    }
+}
+
+function getComputerChoice() {
+    const computedNum = Math.floor(Math.random() * 3)
+    if (computedNum === 0) {
+        return "rock"
+    } else if (computedNum === 1) {
+        return "paper"
+    } else if (computedNum === 2) {
+        return "scissors"
+    }
+}
